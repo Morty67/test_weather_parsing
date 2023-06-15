0ceab776c6b5301513ef1c48b3bc8092a11e9fcf
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-16 01:15:12 +0300

    Added WeatherView and WeatherRun to views.py

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.exceptions import NotFound
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from weather_parser import run_parser
+from .models import Weather
+from .serializers import WeatherSerializer
+
+
+class WeatherView(generics.RetrieveAPIView):
+    serializer_class = WeatherSerializer
+
+    def get_object(self):
+        date_str = self.kwargs["date"]
+        try:
+            return Weather.objects.get(date=date_str)
+        except Weather.DoesNotExist:
+            formatted_date = date_str.strftime("%d-%m-%Y")
+            raise NotFound(
+                f"Weather data for {formatted_date} does not exist."
+            )
+        except ValueError:
+            raise NotFound(
+                f"Invalid date format: {date_str}. Use the format 'dd.mm.yyyy'."
+            )
+
+
+class WeatherRunView(APIView):
+    def post(self, request, format=None):
+        run_parser()
+        return Response(
+            {
+                "status": status.HTTP_200_OK,
+                "message": "Weather information " "updated.",
+            }
+        )
