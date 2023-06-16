fa697dae1713658de9d2931947293cba6a2be0a1
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-16 14:21:15 +0300

    Added new url

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path, register_converter
 
 from weather_app.utils.date_converter import DateConverter
-from weather_app.views import WeatherView, WeatherRunView
+from weather_app.views import (
+    WeatherView,
+    WeatherRunView,
+    ParserTimeSettingsView,
+)
 
 register_converter(DateConverter, "date")
 
 urlpatterns = [
-    path("weather/<date:date>/", WeatherView.as_view(), name="weather"),
+    path("weather/<date:date>/", WeatherView.as_view(), name="weather-view"),
     path("weather/run/", WeatherRunView.as_view(), name="weather-run"),
+    path("parser-settings/", ParserTimeSettingsView.as_view(),
+         name="parser-time-settings"),
+
 ]
 
 app_name = "weather"
