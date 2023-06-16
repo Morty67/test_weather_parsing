85453e86b4aa583bea6a5ee32ea1242b603aeef7
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-16 14:18:13 +0300

    Updated settings.py

--- weather_parsing/settings.py
+++ weather_parsing/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,7 +82,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
