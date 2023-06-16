4138ed8f69c80939cca6502b8ec59442eea44a63
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-16 14:18:55 +0300

    Added ParserTimeSettingsSerializer

--- weather_app/serializers.py
+++ weather_app/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 
-from weather_app.models import Weather
+from weather_app.models import Weather, ParserTimeSettings
 
 
 class WeatherSerializer(serializers.ModelSerializer):
-    date = serializers.DateField(format="%d.%m.%Y")
+    date = serializers.DateField(format="%d-%m-%Y")
 
     class Meta:
         model = Weather
         fields = ["date", "temperature", "weather_description"]
+
+
+class ParserTimeSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ParserTimeSettings
+        fields = ["parser_time"]
